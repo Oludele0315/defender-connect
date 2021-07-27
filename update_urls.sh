d88ebefe8ba14bb000ddc39c39372dbd7c75ce14
Paul Huijbregts <30799281+pahuijbr@users.noreply.github.com>
2021-07-27 15:26:35 -0700

    Create update_urls.sh

---
+++ update_urls.sh
@@ -0,0 +1,11 @@
+sudo curl -o /etc/squid/MDE.destinations.dl https://raw.githubusercontent.com/microsoft/defender-connect/main/MDE.destinations
+if cmp -s "/etc/squid/MDE.destinations.dl" "/etc/squid/MDE.destinations"
+then
+echo "The files match, I'm not going to do anything"
+else
+echo "The files are different so I'm going to replace the Squid file"
+sudo cp /etc/squid/MDE.destinations.dl /etc/squid/MDE.destinations
+echo "Restarting Squid"
+sudo squid -k reconfigure
+echo "Done"
+fi
